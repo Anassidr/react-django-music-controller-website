3e1e74e575811b1267d45d69f76d0d46671fa17f
Anassidr <kamalanass96@gmail.com>
2023-03-14 14:58:47 +0100

    react

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 # Create your views here.
 
 
-def main(request):
-    return HttpResponse("Hello")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()           #what we wanna return
+    serializer_class = RoomSerializer       #how to convert the data into a format that we can use
      
