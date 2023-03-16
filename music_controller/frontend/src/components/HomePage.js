90c697544fae05731a177c2c58ae0b48b26e968f
Anassidr <kamalanass96@gmail.com>
2023-03-17 00:24:48 +0100

    Leaving room

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -19,6 +19,7 @@ export default class HomePage extends Component {
         this.state = {
             roomCode: null,
         };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
     }
 
     async componentDidMount () {
@@ -55,6 +56,12 @@ export default class HomePage extends Component {
 
         );
     }
+
+    clearRoomCode() {
+        this.setState({
+            roomCode: null,
+        });
+    }
     render() {
         return (
         <Router>
@@ -65,7 +72,12 @@ export default class HomePage extends Component {
                 }}/>
                 <Route path="/join" component ={RoomJoinPage} />
                 <Route path="/create" component ={CreateRoomPage} />
-                <Route path="/room/:roomCode" component={Room} />
+                <Route 
+                path="/room/:roomCode"
+                render={(props) => {
+                    return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+                }}
+                />
             </Switch>
         </Router>
         );
