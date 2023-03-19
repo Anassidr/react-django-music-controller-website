a0e7a732000925c416a80e83f7eff8ceb7ea83b1
Anassidr <kamalanass96@gmail.com>
2023-03-19 16:04:59 +0100

    fixing null constraint

--- music_controller/spotify/credentials.py
+++ music_controller/spotify/credentials.py
@@ -1,5 +1,5 @@
 import os
 
 CLIENT_ID = "99af59e06ff84ae89fa482b213840cdb"
-CLIENT_SECRET = os.environ.get['spotify_api_client_secret'] 
-REDIRECT_URI = ""
+CLIENT_SECRET = os.environ.get('spotify_api_client_secret')
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
